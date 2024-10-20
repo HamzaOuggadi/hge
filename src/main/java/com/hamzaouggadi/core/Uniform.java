4894fd5c4ee5a6e4096514d882aaa754e11c9589
Hamza Ouggadi <45943796+HamzaOuggadi@users.noreply.github.com>
2024-10-20 12:24:07 +0100

    //

---
+++ src/main/java/com/hamzaouggadi/core/Uniform.java
@@ -0,0 +1,13 @@
+package com.hamzaouggadi.core;
+
+public class Uniform<T> {
+
+    // Store the data type used in GLSL
+    // int - float - bool - vec2 - vec3 - vec4
+    private String dataType;
+
+    public T data;
+
+
+
+}
